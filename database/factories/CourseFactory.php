25933dbfd66f9be0c2327d5f9dcda063a4337ce3
FerchoEL <lufeesle@gmail.com>
2022-05-12 16:55:51 -0500

    Seeders and factories

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -2,7 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\Course;
+use App\Models\Level;
+use App\Models\Price;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class CourseFactory extends Factory
 {
@@ -13,10 +19,18 @@ class CourseFactory extends Factory
      */
     public function definition()
     {
+        $title = $this->faker->sentence();
         return [
-            'title' => $this->faker->sentence(),
+            'title' => $title ,
             'subtitle' => $this->faker->sentence(),
             'description' => $this->faker->paragraph(),
+            'status' => $this->faker->randomElement([Course::BORRADOR,Course::REVISION,Course::PUBLICADO]),
+            'slug' => Str::slug($title),
+            'user_id' =>User::all()->random()->id,
+            'level_id' =>Level::all()->random()->id,
+            'category_id' =>Category::all()->random()->id,
+            'price_id' =>Price::all()->random()->id,
+
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Platform;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Storage::deleteDirectory('public/cursos');
+        Storage::makeDirectory('public/cursos');
         $this->call(UserSeeder::class);
         $this->call(LevelSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PriceSeeder::class);
+        $this->call(PlatformSeeder::class);
+        $this->call(CourseSeeder::class);
         
     }
 }
